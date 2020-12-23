c4f21a0f700eb3567ee3da26fc3fceb26b6706b7
nj7giveaway02 <72903987+nj7giveaway02@users.noreply.github.com>
2020-12-23 14:04:30 +0530

    Create Footer.vue

---
+++ src/views/common/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="footer">
+    <div class="columns is-mobile is-centered">
+      <div class="field is-grouped is-grouped-multiline">
+        <div class="control">
+          <div class="tags has-addons">
+            <a
+              class="tag is-dark"
+              href="https://nitishjeet7.tk"
+            >nitishjeet7</a>
+            <span class="tag is-light">
+              <span class="icon">
+                <i class="fa fa-code"></i>
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: {},
+  watch: {},
+  data: function () {
+    return {
+      content: ""
+    };
+  },
+  components: {},
+  methods: {}
+};
+</script>
